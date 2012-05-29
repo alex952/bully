a5c31b6a33da59310814d4d7d282d8ae3984d77e
alex952 <alex952.anf5@gmail.com>
2012-05-29 14:32:47 +0200

    Changed the way the program expects a Master msg
        - Change: use of another thread to wait for master messages.
          Instead, the program uses the main thread used for gathering
          election messages to receive master messages as well, but only
          when an election is already casted. The watForMasterMessage now
          only stalls the main thread and check if the master stored in the
          class is the same as before the stall. If it isn't an election is
          casted again.
        - Add: ignore line in gitignore for the build folder.

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,26 +7,26 @@ import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 /**
  * Main class for the Bully algorithm
+ *
  * @author alex952
  */
 public class Main implements Runnable {
+
 	public enum BullyMessages {
+
 		ElectionRequest("election"), ElectionAnswer("electionAnswer"), Master("master");
-		
 		private final String msg;
-		
+
 		BullyMessages(String msg) {
 			this.msg = msg;
 		}
-		
+
 		String message() {
 			return this.msg;
 		}
-		
+
 		static BullyMessages fromMsg(String msg) {
 			if (msg.equals("election")) {
 				return BullyMessages.ElectionRequest;
@@ -39,7 +39,7 @@ public class Main implements Runnable {
 			}
 		}
 	}
-	
+
 	private class ElectionWaitThread implements Runnable {
 
 		public MulticastSocket ms;
@@ -48,17 +48,18 @@ public class Main implements Runnable {
 		private Logger logger = LoggerFactory.getLogger(ElectionWaitThread.class);
 		private String ip;
 		private BullyMessages msgExpected;
-		
+
 		private class ClientSocketThread implements Runnable {
+
 			private Logger logger = LoggerFactory.getLogger(this.getClass());
 			private ElectionWaitThread instance = null;
 			private Socket client = null;
-			
+
 			public ClientSocketThread(Socket client, ElectionWaitThread instance) {
 				this.client = client;
 				this.instance = instance;
 			}
-			
+
 			@Override
 			public void run() {
 				try {
@@ -77,15 +78,14 @@ public class Main implements Runnable {
 					this.logger.error("Error with communication with the client {}", client.getInetAddress().getHostAddress());
 				}
 			}
-
 		}
-		
+
 		public ElectionWaitThread(MulticastSocket ms, String ip, BullyMessages msgExpected) {
 			this.ms = ms;
 			this.ip = ip;
 			this.msgExpected = msgExpected;
 		}
-		
+
 		@Override
 		public void run() {
 			try {
@@ -98,51 +98,45 @@ public class Main implements Runnable {
 					} catch (SocketTimeoutException e) {
 						break;
 					}
-					
+
 					Thread t = new Thread(new ClientSocketThread(client, this));
 					t.start();
 				}
 				/*
-				byte[] buf = new byte[256];
-				DatagramPacket dp = new DatagramPacket(buf, buf.length);
-				
-				try {
-					while(!this.responseReceived) {
-						ms.receive(dp);
-						
-						String sourceIp = dp.getAddress().getHostAddress();
-						if (this.ip.equals(sourceIp))
-							continue;
-						
-						this.logger.info("Received election response from {}", sourceIp);
-						
-						String msg = new String(dp.getData());
-						msg = msg.trim();
-						BullyMessages msgBully = BullyMessages.fromMsg(msg);
-
-						if (msgBully == this.msgExpected) {
-							this.responseReceived = true;
-							this.responsePacket = dp;
-						}
-					}
-				} catch (IOException ex) {
-					this.logger.info("Closing socket");
-				}*/
+				 * byte[] buf = new byte[256]; DatagramPacket dp = new
+				 * DatagramPacket(buf, buf.length);
+				 *
+				 * try { while(!this.responseReceived) { ms.receive(dp);
+				 *
+				 * String sourceIp = dp.getAddress().getHostAddress(); if
+				 * (this.ip.equals(sourceIp)) continue;
+				 *
+				 * this.logger.info("Received election response from {}",
+				 * sourceIp);
+				 *
+				 * String msg = new String(dp.getData()); msg = msg.trim();
+				 * BullyMessages msgBully = BullyMessages.fromMsg(msg);
+				 *
+				 * if (msgBully == this.msgExpected) { this.responseReceived =
+				 * true; this.responsePacket = dp; } } } catch (IOException ex)
+				 * { this.logger.info("Closing socket");
+				}
+				 */
 			} catch (IOException ex) {
 				java.util.logging.Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 			}
 		}
-		
 	}
-	
+
 	private class MessageWaitThread implements Runnable {
+
 		private MulticastSocket ms;
 		private InetAddress group;
 		private int port;
 		private String ip;
 		private Main instance;
 		private Logger logger = LoggerFactory.getLogger(MessageWaitThread.class);
-		
+
 		public MessageWaitThread(MulticastSocket ms, String ip, Main instance, InetAddress group, int port) {
 			this.ms = ms;
 			this.ip = ip;
@@ -150,7 +144,7 @@ public class Main implements Runnable {
 			this.group = group;
 			this.port = port;
 		}
-		
+
 		@Override
 		public void run() {
 			while (true) {
@@ -158,11 +152,12 @@ public class Main implements Runnable {
 				DatagramPacket dp = new DatagramPacket(buf, buf.length);
 				try {
 					this.ms.receive(dp);
-					
+
 					String sourceIp = dp.getAddress().getHostAddress();
-					if (this.ip.equals(sourceIp))
+					if (this.ip.equals(sourceIp)) {
 						continue;
-					
+					}
+
 					String msg = new String(dp.getData());
 					msg = msg.trim();
 					BullyMessages bullyMsg = BullyMessages.fromMsg(msg);
@@ -174,10 +169,14 @@ public class Main implements Runnable {
 						byte[] respB = resp.getBytes();
 						DatagramPacket respP = new DatagramPacket(respB, respB.length, this.group, this.port);
 						this.ms.send(respP);
-						
+
 						this.instance.election();
 					} else if (bullyMsg == BullyMessages.ElectionRequest && this.ip.compareTo(sourceIp) < 0) {
 						this.logger.info("Received election message from a following ip ({}), not answering", sourceIp);
+					} else if (bullyMsg == BullyMessages.Master) {
+						if (this.instance.electionCasted == true) {
+							this.instance.master = sourceIp;
+						}
 					}
 				} catch (SocketTimeoutException ex) {
 					continue;
@@ -187,32 +186,33 @@ public class Main implements Runnable {
 			}
 		}
 	}
-	
 	private final Logger logger = LoggerFactory.getLogger(Main.class);
 	private MulticastSocket ms;
 	private InetAddress group;
 	private String ip;
+	private String master = "";
 	private Boolean electionCasted = false;
 	private int port = 4443;
 	private String groupIp = "224.0.0.1";
-	
 	private Thread waitMessagesThread = null;
 	private Thread masterTask = null;
-	
-	public Main() {
+
+	public Main(Runnable masterTask) {
 		try {
 			//Find out own ip
 			InetAddress ownAddress = InetAddress.getLocalHost();
 			ip = ownAddress.getHostAddress();
 			this.logger.info("I'm node with ip {} and i'm joining the multicast group", this.ip);
-			
+
 			//Join multicast group
 			ms = new MulticastSocket(4663);
 			ms.setSoTimeout(5000);
 			group = InetAddress.getByName(this.groupIp);
 			ms.joinGroup(group);
-			
-			
+
+			//Create thread of master task
+			this.masterTask = new Thread(masterTask);
+
 			Thread.sleep(2000L);
 		} catch (UnknownHostException e) {
 			this.logger.error("Cannot get you local ip address");
@@ -222,38 +222,40 @@ public class Main implements Runnable {
 			this.logger.error("Error pausing thread");
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		this.logger.info("Casting raise election");
 		this.election();
-		
+
 		this.logger.info("Listening now on {}:{} for election messages from the multicast group", this.groupIp, this.port);
 		this.waitMessagesThread = new Thread(new MessageWaitThread(this.ms, this.ip, this, this.group, this.port));
 		this.waitMessagesThread.start();
 	}
-	
+
 	private void election() {
-		if (electionCasted)
+		if (electionCasted) {
 			return;
-		
+		}
+
 		String msg = BullyMessages.ElectionRequest.message();
 		byte[] buf = msg.getBytes();
-		
+
 		DatagramPacket dp = new DatagramPacket(buf, buf.length, this.group, this.port);
 		try {
 			ElectionWaitThread electionThread = new ElectionWaitThread(ms, ip, BullyMessages.ElectionAnswer);
 			Thread th = new Thread(electionThread);
 			th.start();
-			
+
 			this.logger.info("Sending election request message");
 			this.ms.send(dp);
 			this.electionCasted = true;
-			
+
 			this.logger.info("Waiting for answers ({} seconds, more or less)", 5);
-			
-			while(th.isAlive()) { }
-			
+
+			while (th.isAlive()) {
+			}
+
 			if (!electionThread.responseReceived) {
 				this.logger.info("No answers received. Sending master message");
 				this.masterMessage();
@@ -261,91 +263,78 @@ public class Main implements Runnable {
 				this.logger.info("Answers received");
 				this.waitForMaster();
 			}
-			
+
 		} catch (IOException ex) {
 			this.logger.error("Error sending multicast eleciton message");
 		}
 	}
-	
+
 	private void waitForMaster() {
 		try {
-			ElectionWaitThread masterWaitThread = new ElectionWaitThread(ms, ip, BullyMessages.ElectionAnswer);
-			Thread th = new Thread(masterWaitThread);
-			th.start();
-			
 			this.logger.info("Waiting for master messages");
+			String oldMaster = this.master;
+
 			Thread.sleep(5000L);
 
-			if (th.isAlive()) {
-				//this.ms.close();
-				th.interrupt();
-				this.ms = new MulticastSocket(port);
-				this.ms.joinGroup(this.group);
-			}
-			
-			if (!masterWaitThread.responseReceived) {
+			if (!this.master.equals(oldMaster)) {
+				this.logger.info("Master message received. The new master is {}", this.master);
+				this.electionCasted = false;
+			} else {
 				this.logger.info("No master message received. Re-casting election");
 				this.election();
-			} else {
-				this.logger.info("Master message received. The new master is {}", new String(masterWaitThread.responsePacket.getAddress().getAddress()));
-				this.electionCasted = false;
-				this.masterTask.interrupt();
-				this.masterTask = null;
 			}
 		} catch (InterruptedException e) {
 			this.logger.error("Couldn't wait for answers due to an error", e);
-		} catch (IOException e) {
-			this.logger.error("Error closing the socket");
 		}
 	}
-	
+
 	private void masterMessage() {
 		String msg = BullyMessages.Master.message();
 		byte[] buf = msg.getBytes();
 		DatagramPacket dp = new DatagramPacket(buf, buf.length, this.group, this.port);
-		
+
 		try {
 			this.logger.info("Sending master message to multicast group");
 			this.ms.send(dp);
-			
-			this.logger.info("Starting task of master");
-			//Start task of master
-			this.masterTask = new Thread(new Runnable() {
-				private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-				@Override
-				public void run() {
-					InetAddress addr;
-					try {
-						addr = InetAddress.getLocalHost();
-					} catch (UnknownHostException ex) {
-						this.logger.error("Stupid bug");
-						return;	
-					}
-					
-					String ip = addr.getHostAddress();
-					
-					while(true) {
-						this.logger.info("I'm the master with ip {}", ip);
-						try {
-							Thread.sleep(2000L);
-						} catch (InterruptedException e) {
-							this.logger.error("Stupid bug");
-							return;
-						}
-					}
-				}
-			});
-			
+			this.logger.info("Starting task of master");
+			//Start task of master	
 			this.masterTask.start();
+
 			this.electionCasted = false;
 		} catch (IOException e) {
 			this.logger.error("Couldn't send master message due to an error", e);
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		Main m = new Main();
+		Main m = new Main(new Runnable() {
+
+			private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+			@Override
+			public void run() {
+				InetAddress addr;
+				try {
+					addr = InetAddress.getLocalHost();
+				} catch (UnknownHostException ex) {
+					this.logger.error("Stupid bug");
+					return;
+				}
+
+				String ip = addr.getHostAddress();
+
+				while (true) {
+					this.logger.info("I'm the master with ip {}", ip);
+					try {
+						Thread.sleep(2000L);
+					} catch (InterruptedException e) {
+						this.logger.error("Stupid bug");
+						return;
+					}
+				}
+			}
+		});
 		m.run();
 	}
 }
